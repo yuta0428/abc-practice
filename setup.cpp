b06e3850cc0de2f14178936bb9db173807e86b93
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2019-10-13 00:08:05 +0900

    Update setup.cpp

--- setup.cpp
+++ setup.cpp
@@ -3,27 +3,35 @@ using namespace std;
 
 int main()
 {
-    char cmd[1024];
     int n; // コンテスト番号 001
-    scanf("%d", &n);
-    char dir[32];
-    sprintf(dir, "./%03d", n);
+    cin >> n;
 
-    sprintf(cmd, "test -d %s\n", dir);
-    int status = system(cmd);
-    if (status!=0){
-        // create
-        sprintf(cmd, "cp -r ./template %s\n", dir);
-        printf(cmd);
-        system(cmd);
-    }
+    char dir_abcd[4] = { 'a', 'b', 'c', 'd' };
+    for (int i = 0; i < 4; i++)
+    {
+        const char dir = dir_abcd[i];
+        ostringstream ss;
 
-    // DL test
-    char c[4] = {'a','b','c','d'};
-    for (int i=0; i<4; i++){
-        sprintf(cmd, "oj d -d %s/%c/test https://atcoder.jp/contests/abc%03d/tasks/abc%03d_%d\n",dir,c[i],n,n,i+1);
-        printf(cmd);
+        // make directories
+        ss << "./";
+        ss << std::setw(3) << std::setfill('0') << n << "/";
+        ss << dir;
+        string cmd1 = "mkdir -p " + ss.str();
+        cout << cmd1 << endl;
+        system(cmd1.c_str());
 
-        system(cmd);
+        // copy template.cpp
+        string file_path = ss.str() + "/" + dir + ".cpp";
+        string cmd2 = "cp ./template/template.cpp " + file_path;
+        cout << cmd2 << endl;
+        system(cmd2.c_str());
+
+        // execute oj d
+        char atcoder_url[256];
+        sprintf(atcoder_url, "https://atcoder.jp/contests/abc%03d/tasks/abc%03d_%d", n, n, i+1);
+        string test_name = ss.str() + "/test";
+        string cmd3 = "oj d -d " + test_name + " " + atcoder_url;
+        cout << cmd3 << endl;
+        system(cmd3.c_str());
     }
 }
--- template/template.cpp
+++ template/template.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define repeat(i, n) for (int i = 0; (i) < (n); ++(i))
+
 int main()
 {
     int a;
