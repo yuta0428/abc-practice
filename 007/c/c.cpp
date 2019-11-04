a5dfe720148f5539a0448d72d7b19e24bd5ab170
yuta0428 <yuta_okamoto428@yahoo.co.jp>
2019-11-04 23:30:49 +0900

    007 d途中まで

--- 007/c/c.cpp
+++ 007/c/c.cpp
@@ -11,5 +11,39 @@ const int INF = 1001001001;
 
 int main()
 {
+    int R, C, sy, sx, gy, gx;
+    cin >> R >> C;
+    cin >> sy >> sx;
+    cin >> gy >> gx;
+    char c[R][C];
+    int b[R][C];
+    repeat(i, R) repeat(j, C) {
+        cin >> c[i][j];
+        b[i][j] = c[i][j] == '.' ? -1 : INF;
+    }
+    queue<tuple<int, int>> q;
+    q.push(make_tuple(sy-1, sx-1));
+    int ans = 0;
+    int v[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    while (!q.empty()) {
+        ans++;
+        queue<tuple<int, int>> tmpq = q;
+        while (!q.empty()) q.pop();
+        while (!tmpq.empty()) {
+            auto t = tmpq.front();
+            tmpq.pop();
+            int x, y;
+            tie(y, x) = t;
+            repeat(i, 4)
+            {
+                int vy = y + v[i][0];
+                int vx = x + v[i][1];
+                if (b[vy][vx] != -1) continue;
+                b[vy][vx] = ans;
+                q.push(make_tuple(vy, vx));
+            }
+        }
+    }
+    cout << b[gy - 1][gx - 1] << endl;
     return 0;
 }
--- 007/d/d.cpp
+++ 007/d/d.cpp
@@ -11,5 +11,25 @@ const int INF = 1001001001;
 
 int main()
 {
+    int A, B;
+    cin >> A >> B;
+    int ans = 0;
+    std::regex re("^4|^9");
+    std::smatch m;
+    foreach(i,A, B){
+        string s = to_string(i);
+        if (regex_search(s, m, re)) {
+            cout << s << ", ";
+            ans++;
+        }
+    }
+    cout << endl;
+    cout << ans << endl;
     return 0;
 }
+
+/*
+一桁目が4or9なのは2*(B/10...四捨五入)個
+40,90,340,390など40,90連番台は2*(B/10...四捨五入)個
+法則性がありそうだけどムズい、、
+*/
